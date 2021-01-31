ce1e545becda8a236cfe566a30c39c7cee9d66fc
leha-code <71947459+leha-code@users.noreply.github.com>
2021-01-30 21:02:08 -0500

    Initial commit

---
+++ TowerTrail.py
@@ -0,0 +1,18 @@
+from mcpi.minecraft import Minecraft
+from mcpi.block import *
+mc = Minecraft.create()
+
+
+
+x,y,z = mc.player.getTilePos()
+new_x = x
+new_z = z
+while True:
+    x,y,z = mc.player.getTilePos()
+    mc.setBlocks(x + 2, y + 10, z + 2, x - 2,y - 2, z - 2, STONE_BRICK.id)
+    mc.setBlocks(x + 1, y + 10, z + 1, x - 1,y - 1, z - 1, AIR.id)
+    mc.setBlocks(x + 2, y + 9, z + 2, x - 2,y + 9, z - 2, MOSS_STONE.id)
+    mc.setBlock(x + 1, y + 9, z + 1, AIR.id)
+    while x != new_x or z != new_z:
+        new_x,y,new_z = mc.player.getTilePos()
+        
